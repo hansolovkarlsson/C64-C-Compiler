b56416c05831842de7e61eddd395291077c665ef
Hans Karlsson <hanskarlsson@me.com>
2026-07-15 07:01:31 -0700

    Removed binaries

---
+++ bin/README.md
@@ -0,0 +1,4 @@
+README.md
+
+# For this project to work, build c64asm from my other project first and put the assembler binary in this directory
+
--- buildall.sh
+++ buildall.sh
@@ -1,4 +1,4 @@
-for f in hello features forward pointers; do
+for f in hello features forward pointers recursion; do
     ./bin/cc64 tests/$f.c -o tests/$f.asm
     ./bin/c64asm tests/$f.asm -o tests/$f.prg --listing tests/$f.lst
 #    python3 bin/mini6502.py tests/$f.prg tests/$f.lst
